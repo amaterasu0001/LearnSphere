4681858c6ea407aa82c31101b9748fef3715645f
mahir2995 <139786025+mahir2995@users.noreply.github.com>
2025-01-24 12:36:14 +0600

    pages
    Creating pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <Link to='/'>Home </Link>
+      <Link to='/AboutUs'>AboutUs </Link>
+      <Link to='/Login'>Login </Link>
+      <Link to='/Profile'>Profile </Link>
+      <Link to='/SignUp'>SignUp </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>This is Sign up</div>;
+};
+
+export default SignUp;
